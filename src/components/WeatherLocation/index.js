c3d2eb9043cf6ee0d905ef9ac37d124b378eed80
Juan <juanopizzi@gmail.com>
2020-05-25 23:28:54 -0300

    desharcodeo la ciudad inicial como buenos aires

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { PropTypes } from 'prop-types';
+import getUrlWeatherByCity from '../../services/getUrlWeatherByCity';
 import transformWeather from "./../../services/transformWeather";
 // se pone llaves cuando al exportar no se utiliza la palabra "default"
-import { api_weather } from './../../constants/api_url';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
 
 class  WeatherLocation extends Component {
 
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
+		const { city } = props;
 		// this.state es el estado local (parcial) de nuestro componente que
 		// va a ayudar que nuestro componente se renderice
 		//? con 'this.' hago referencia a cosas que son propias del componente
 		this.state = {
-			city: 'Buenos Aires',
+			city,
 			data: null,
 		}
 	}
@@ -33,6 +36,7 @@ class  WeatherLocation extends Component {
 	handleUpdateClick = () => {
 		// fetch trae los datos del server para poder
 		// usarlos en nuestro navegador
+		const api_weather = getUrlWeatherByCity(this.state.city);
 		fetch(api_weather).then( resolve => {
 			// esto va a devolver una promises, con los datos del clima que
 			// recibo del server, sino hago esto solo voy a ver la informacion
@@ -40,8 +44,8 @@ class  WeatherLocation extends Component {
 			return resolve.json();
 		}).then(data => {
 			const newWeather = transformWeather(data);
-			console.log(newWeather);
-			debugger;
+			//console.log(newWeather);
+			//debugger;
 			//! si no uso setState, la informacion no se actualiza nunca
 			// No hace falta pasar todos los datos, solo los que se van a cambiar
 			// es decir, no le paso city porque no lo voy a cambiar
@@ -60,11 +64,15 @@ class  WeatherLocation extends Component {
 				<Location city={city}></Location>
 				{data ?
 					<WeatherData data={data}></WeatherData> :
-					"Cargando..."
+					<CircularProgress size={50}/>
 				}
 			</div>
 		);
 	}
+}
+
+WeatherLocation.propTypes = {
+	 city: PropTypes.string.isRequired,
 }
 
 export default WeatherLocation;

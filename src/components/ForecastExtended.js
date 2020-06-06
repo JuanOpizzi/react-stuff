e0fcfb042aa083c390b50dcced795cd03ecec728
Juan <juanopizzi@gmail.com>
2020-06-05 22:22:50 -0300

    refactor de forecastExtended
    Ahora cuando clickeas ciudades distintas SI carga el pronostico extendido de esa ciudad (lo que hacia antes era mostrar solo el clima extendido de la primer ciudad clickeada)

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -4,22 +4,6 @@ import ForecastItem from './ForecastItem';
 import transformForecast from './../services/transformForecast';
 import './styles.css';
 import WeatherData from './WeatherLocation/WeatherData';
-/*
-const days = [
-  'Lunes',
-  'Martes',
-  'Miercoles',
-  'Jueves',
-  'Viernes',
-];
-
-const data = {
-  temperature: 10,
-  humidity: 10,
-  weatherState: 'normal',
-  wind: 'normal',
-};
-*/
 
 export const api_key = "b6b1125383df7195a4df361789c39110";
 export const url = "http://api.openweathermap.org/data/2.5/forecast";
@@ -31,16 +15,34 @@ class ForecastExtended extends Component {
     this.state = { forecastData: null }
   }
 
+  //? (5) Setando forecastData a null lo que logro es que aparezca el indicador de carga.
+
+  //* Esta funcion de ejecuta cada vez que hay alguna actualizacion de las propiedades.
+  //* Recibe por parametro las nuevas propiedades que se van a establecer.
+  //* Es un punto previo a la actualizacion de las propiedades y del componente.
+
+  //* Tener en cuenta que esta funcion solo se ejecuta siempre que se modifiquen las
+  //* propiedades excepto la primer vez que se establece el componente
+  //* (por eso tambien ejecuto updateCity en componenteWillMount, porque sino no se
+  //* ejecuta una primer vez).
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.city !== this.props.city) {
+      this.setState({ forecastData: null }) //! (5)
+      this.updateCity(nextProps.city);
+    }
+  }
+  
+
   //? (1) Hacemos un fetch al server, primero obtenemos la informacion que viene del server
   //? (2) Ejecutamos lo que nos da los datos en formato JSON
   //? (3) Obtenemos el weatherData, lo mostramos por pantalla. Cuando obtuvimos estos
   //?     datos del server, los transformamos en un objeto y seteamos el estado
-  //* El componentDidMount se ejecuta una unica vez en el momento que se esta generando
-  //*  el componente por primera vez
-  componentDidMount() {
-    const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
 
-    fetch(url_forecast).then (  //! (3)
+  updateCity = city => {
+    const url_forecast = `${url}?q=${city}&appid=${api_key}`;
+  
+    fetch(url_forecast).then (  //! (1)
       data => (data.json())     //! (2)
     ).then(
       weather_data => {         //! (3)
@@ -51,8 +53,15 @@ class ForecastExtended extends Component {
       }
     );
   }
-  // Va a devolver un array con los componentes de ForecastItem por dia
-  // Uso como key el dia de la semana y la hora
+
+  //* El componentDidMount se ejecuta una unica vez en el momento que se esta generando
+  //* el componente por primera vez
+  componentDidMount() {
+    this.updateCity(this.props.city);
+  }
+
+  //* Esta funcion va a devolver un array con los componentes de ForecastItem por dia
+  //* Uso como key el dia de la semana y la hora
   renderForecastItemDays(forecastData) {
     return forecastData.map( forecast => (
       <ForecastItem
@@ -68,13 +77,14 @@ class ForecastExtended extends Component {
     return <h3>Cargando pronostico extendido...</h3>;
   }
 
-  //? (1) En vez de tomarlo de las props como lo hicismos con city, lo vamos a tomar del 
+  //? (4) En vez de tomarlo de las props como lo hicismos con city, lo vamos a tomar del 
   //?     state porque es lo que nos va a tomar el servidor, no va a hacer una propiedad
   //?     que se inyecte desde un componente externo sino se va a encargar el mismo 
   //?     componente de obtenerla y establecerla en el state
+
   render() {
     const { city } = this.props;
-    const { forecastData } = this.state; //! (1)
+    const { forecastData } = this.state; //! (4)
     return (
       <div>
         <h2 className='forecast-title'>Pronostico Extendido para {city}</h2>
